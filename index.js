285eb707ef6d5b86ff2f7b3f33471292e711305c
hinoobers <reio.poldmaa@voco.ee>
2025-02-20 08:48:38 +0200

    Installed basic packages

---
+++ index.js
@@ -0,0 +1,16 @@
+const mysql2 = require("mysql2");
+const sequelize = require("sequelize");
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.json({message: "Hi!"})
+});
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+})
