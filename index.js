6c4af49f7b8a3b7fb184c3e703bde5b2462c7d68
hinoobers <reio.poldmaa@voco.ee>
2025-02-20 09:01:17 +0200

    Created connection to database

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const mysql2 = require("mysql2");
-const sequelize = require("sequelize");
+const Sequelize = require("sequelize");
 const express = require("express");
 
 const app = express();
@@ -7,10 +7,32 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const sequelize = new Sequelize("mysql://root@localhost:3306/joga_sequelize");
+sequelize.authenticate().then(() => {
+    console.log("Connection has been established successfully.");
+}).catch(err => {
+    console.error("Unable to connect to the database:", err);
+});
 app.get("/", (req, res) => {
     res.json({message: "Hi!"})
 });
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+// get all articles
+app.get("/article", (req, res) => {
+
+});
+
+// get article by slug
+app.get("/article/:slug", (req, res) => {
+
+});
+
+// get author’s articles by author id
+app.get("/author/:id", (req, res) => {
+
+});
+
+
+app.listen(3001, () => {
+    console.log("Server is running on port 3001");
 })
